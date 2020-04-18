2e6084f0671bc0c262824da6dcf9dccb76141f66
Jings <c34064054@gs.ncku.edu.tw>
2020-04-18 13:53:24 +0800

    complete

--- HW2_Morphing/morphing.cpp
+++ HW2_Morphing/morphing.cpp
@@ -4,28 +4,32 @@
 #include <cstdlib>
 #include <cmath>
 
+#define img1_x 50
+#define img1_y 70
+#define img2_x 500
+#define img2_y 70
+
 using namespace cv;
 using namespace std;
 
 Mat OutputImage = Mat::zeros(Size(800, 300), CV_8UC3);
 Mat image1, image2, morph_button;
 
-Point des1, des2, src1, src2;
-Point des_initial(50, 70), source_initial(500, 70);
-
-bool des1_true, des2_true, source1_true, source2_true;
-bool button_true = false;
-
-int des_arrow = 0, source_arrow = 0;
-
+Point image1_start, image1_end, image2_start, image2_end;
+Point image1_original (img1_x,img1_y), image2_original(img2_x, img2_y);
 Point P[50], Q[50], P_prime[50], Q_prime[50];
+
+bool img1_start_check = false, img1_end_check = false;
+bool img2_start_check = false, img2_end_check = false, button_true = false;
+
+int image1_arrow_index = 0, image2_arrow_index = 0;
 
 Point perpendicular(Point p) {
 	Point transform(p.y, -p.x);
 	return transform;
 }
 
-void StartToMorph() {
+void morphing() {
 
 	cv::namedWindow("t = 0", 1);
 	cv::namedWindow("t = 0.25", 1);
@@ -63,9 +67,9 @@ void StartToMorph() {
 	mapx_34_2.create(image1.size(), CV_32FC1);
 	mapy_34_2.create(image1.size(), CV_32FC1);
 
-	for (int x = 0; x < 255; x++) {
+	for (int x = 0; x < image1.cols; x++) {
 		// 2->1
-		for (int y = 0; y < 189; y++) {
+		for (int y = 0; y < image1.rows; y++) {
 			X.x = x;
 			X.y = y;
 			XSum.x = 0;
@@ -75,7 +79,10 @@ void StartToMorph() {
 			weight = 0;
 			WeightSum = 0.0;
 
-			arrow_num = (des_arrow > source_arrow) ? source_arrow : des_arrow;
+			if (image1_arrow_index > image2_arrow_index)
+				arrow_num = image2_arrow_index;
+			else
+				arrow_num = image1_arrow_index;
 
 			for (int z = 0; z < arrow_num; z++) {
 
@@ -108,14 +115,14 @@ void StartToMorph() {
 			}
 			mapx_14.at<float>(y, x) = (((XSum / WeightSum) - X)*0.75 + X).x;
 			mapy_14.at<float>(y, x) = (((XSum / WeightSum) - X)*0.75 + X).y;
-			mapx_24.at<float>(y, x) = (((XSum / WeightSum) - X)*0.5 + X).x;
-			mapy_24.at<float>(y, x) = (((XSum / WeightSum) - X)*0.5 + X).y;
+			mapx_24.at<float>(y, x) = (((XSum / WeightSum) - X)*0.5  + X).x;
+			mapy_24.at<float>(y, x) = (((XSum / WeightSum) - X)*0.5  + X).y;
 			mapx_34.at<float>(y, x) = (((XSum / WeightSum) - X)*0.25 + X).x;
 			mapy_34.at<float>(y, x) = (((XSum / WeightSum) - X)*0.25 + X).y;
 		}
 
 		// 1->2
-		for (int y = 0; y < 189; y++) {
+		for (int y = 0; y < image1.rows; y++) {
 			X.x = x;
 			X.y = y;
 			XSum.x = 0;
@@ -163,7 +170,7 @@ void StartToMorph() {
 			mapy_34_2.at<float>(y, x) = (((XSum / WeightSum) - X)*0.75 + X).y;
 		}
 	}
-
+	
 	remap(image2, two2one_14, mapx_14, mapy_14, INTER_LINEAR);
 	remap(image2, two2one_24, mapx_24, mapy_24, INTER_LINEAR);
 	remap(image2, two2one_34, mapx_34, mapy_34, INTER_LINEAR);
@@ -171,9 +178,9 @@ void StartToMorph() {
 	remap(image1, one2two_14, mapx_14_2, mapy_14_2, INTER_LINEAR);
 	remap(image1, one2two_24, mapx_24_2, mapy_24_2, INTER_LINEAR);
 	remap(image1, one2two_34, mapx_34_2, mapy_34_2, INTER_LINEAR);
-
+	
 	addWeighted(one2two_14, 0.75, two2one_14, 0.25, 0.0, mix_14);
-	addWeighted(one2two_24, 0.5, two2one_24, 0.5, 0.0, mix_24);
+	addWeighted(one2two_24, 0.5,  two2one_24, 0.5 , 0.0, mix_24);
 	addWeighted(one2two_34, 0.25, two2one_34, 0.75, 0.0, mix_34);
 
 	cv::imshow("t = 0", image1);
@@ -186,55 +193,58 @@ void StartToMorph() {
 void CallBack(int event, int x, int y, int flags, void* userdata)
 {
 	// image1
-	if (x >= 50 && x <= 305 && y >= 70 && y <= 259)
+	if (x >= img1_x && x <= (img1_x+image1.cols) && y >= img1_y && y <= (img1_y+image1.rows))
 	{
 		// press mouse
 		if (event == EVENT_LBUTTONDOWN)
 		{
-			std::cout << "pressed at (" << x - des_initial.x << ", " << y - des_initial.y << ")" << endl;
-			des1 = Point(x, y);
-			des1_true = true;
+			std::cout << "in image1" << endl;
+			std::cout << "start at (" << x - image1_original.x << ", " << y - image1_original.y << ")" << endl;
+			image1_start = Point(x, y);
+			img1_start_check = true;
 		}
 		// release mouse
 		else if (event == EVENT_LBUTTONUP)
 		{
-			std::cout << "released at (" << x - des_initial.x << ", " << y - des_initial.y << ")" << endl;
-			des2 = Point(x, y);
-			des2_true = true;
-			if (des1_true == true && des2_true == true) {
+			std::cout << "end at (" << x - image1_original.x << ", " << y - image1_original.y << ")" << endl;
+			image1_end = Point(x, y);
+			img1_end_check = true;
+			if (img1_start_check == true && img1_end_check == true) {
 				// draw the arrowed line 
-				arrowedLine(OutputImage, des1, des2, Scalar(255, 255, 255), 2);
-				P[des_arrow] = des1 - des_initial;
-				Q[des_arrow] = des2 - des_initial;
-				des_arrow++;
-				des1_true = false;
-				des2_true = false;
+				arrowedLine(OutputImage, image1_start, image1_end, Scalar(255, 255, 255), 2);
+				P[image1_arrow_index] = image1_start - image1_original;
+				Q[image1_arrow_index] = image1_end - image1_original;
+				image1_arrow_index++;
+				img1_start_check = false;
+				img1_end_check = false;
 			}
 		}
 	}
 	// image2 
-	else if (x >= 500 && x <= 755 && y >= 70 && y <= 259)
+	else if (x >= img2_x && x <= (img2_x+image2.cols) && y >= img2_y && y <= (img2_y+image2.rows))
 	{
 		// press mouse 
 		if (event == EVENT_LBUTTONDOWN)
 		{
-			std::cout << "pressed at (" << x - source_initial.x << ", " << y - source_initial.y << ")" << endl;
-			src1 = Point(x, y);
-			source1_true = true;
+			std::cout << "in image2" << endl;
+			std::cout << "start at (" << x - image2_original.x << ", " << y - image2_original.y << ")" << endl;
+			image2_start = Point(x, y);
+			img2_start_check = true;
 		}
 		// release mouse
 		else if (event == EVENT_LBUTTONUP)
 		{
-			std::cout << "released at (" << x - source_initial.x << ", " << y - source_initial.y << ")" << endl;
-			src2 = Point(x, y);
-			source2_true = true;
-			if (source1_true == true && source2_true == true) {
-				arrowedLine(OutputImage, src1, src2, Scalar(255, 255, 255), 2);
-				P_prime[source_arrow] = src1 - source_initial;
-				Q_prime[source_arrow] = src2 - source_initial;
-				source_arrow++;
-				source1_true = false;
-				source2_true = false;
+			std::cout << "end at (" << x - image2_original.x << ", " << y - image2_original.y << ")" << endl;
+			image2_end = Point(x, y);
+			img2_end_check = true;
+
+			if (img2_start_check == true && img2_end_check == true) {
+				arrowedLine(OutputImage, image2_start, image2_end, Scalar(255, 255, 255), 2);
+				P_prime[image2_arrow_index] = image2_start - image2_original;
+				Q_prime[image2_arrow_index] = image2_end - image2_original;
+				image2_arrow_index++;
+				img2_start_check = false;
+				img2_end_check = false;
 			}
 		}
 	}
@@ -244,28 +254,18 @@ void CallBack(int event, int x, int y, int flags, void* userdata)
 		if (event == EVENT_LBUTTONDOWN)
 			button_true = true;
 	}
-	// just show the path
-	else if (event == EVENT_RBUTTONDOWN)
-	{
-		std::cout << "destination:" << endl;
-		for (int a = 0; a < des_arrow; a++)
-			std::cout << "(" << P[a].x << "," << P[a].y << ") to (" << Q[a].x << "," << Q[a].y << ")" << endl;
 
-		std::cout << "source:" << endl;
-		for (int a = 0; a < source_arrow; a++)
-			std::cout << "(" << P_prime[a].x << "," << P_prime[a].y << ") to (" << Q_prime[a].x << "," << Q_prime[a].y << ")" << endl;
-	}
-	// press the morphing button
 	if (button_true == true)
 	{
 		button_true = false;
-		StartToMorph();
+		morphing();
 	}
 	cv::imshow("Morphing", OutputImage);
 }
 
 int main(int argc, char** argv)
 {
+	OutputImage.setTo(Scalar(100, 0, 0));
 	image1 = imread("women.jpg");
 	image2 = imread("cheetah.jpg");
 	morph_button = imread("morph_button.png");
@@ -273,19 +273,22 @@ int main(int argc, char** argv)
 
 	if (!image1.data || !image2.data || !morph_button.data)
 	{
-		printf("image load error \n");
+		std::cout << "load image error" << endl;
 		exit(-1);
 	}
 
-	Rect R1(50, 70, image1.cols, image1.rows);
-	Rect R2(500, 70, image2.cols, image2.rows);
+	Rect R1(img1_x, img1_y, image1.cols, image1.rows);
+	Rect R2(img2_x, img2_y, image2.cols, image2.rows);
 	Rect button_R(325, 70, 160, 80);
 	image1.copyTo(OutputImage(R1));
 	image2.copyTo(OutputImage(R2));
 	morph_button.copyTo(OutputImage(button_R));
 
-	cv::putText(OutputImage, "left image", Point(120, 50), FONT_HERSHEY_COMPLEX_SMALL, 1, Scalar(255, 255, 255), 2);
-	cv::putText(OutputImage, "right image", Point(550, 50), FONT_HERSHEY_COMPLEX_SMALL, 1, Scalar(255, 255, 255), 2);
+	std::cout << "img1 row: " << image1.rows << " col: " << image1.cols << endl;
+	std::cout << "img2 row: " << image2.rows << " col: " << image2.cols << endl;
+
+	cv::putText(OutputImage, "image1", Point(130, 50), FONT_HERSHEY_COMPLEX_SMALL, 1, Scalar(255, 255, 255), 2);
+	cv::putText(OutputImage, "image2", Point(580, 50), FONT_HERSHEY_COMPLEX_SMALL, 1, Scalar(255, 255, 255), 2);
 
 	cv::namedWindow("Morphing", 1);
 	cv::setMouseCallback("Morphing", CallBack, NULL);
